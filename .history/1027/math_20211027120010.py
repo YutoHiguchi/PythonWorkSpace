210f5fc3c618ab786b24634400323df3168a5c52
Yuto Higuchi <gutchan915@gmail.com>
2021-10-27 12:05:07 +0900

    comit

---
+++ .history/1027/math_20211027120010.py
@@ -0,0 +1,19 @@
+'''
+【実行例】
+
+> python math.py
+角度を入力してください（度）： 45
+45.0 度->0.7853981633974483 ラジアン
+sin(45.0)=>0.7071067811865475
+
+'''
+
+import math
+input_angle = float(input('角度を入力してください（度）： '))
+print(f"{input_angle}度 -> {math.radians(input_angle)}ラジアン")
+
+print(f"sin({input_angle}) => {math.sin(input_angle)}")
+
+
+
+
---
+++ .history/1027/math_20211027120352.py
@@ -0,0 +1,29 @@
+'''
+【実行例】
+
+> python math.py
+角度を入力してください（度）： 45
+45.0 度->0.7853981633974483 ラジアン
+sin(45.0)=>0.7071067811865475
+
+'''
+
+import math
+#モジュールのインポート
+
+input_angle = float(input('角度を入力してください（度）： '))
+
+angle = input_angle * math.pi / 180
+
+print(f"{input_angle}度 -> {math.radians(input_angle)}ラジアン")
+
+# --先生の回答--
+print(f"{input_angle}度 -> {angle}ラジアン")
+#---
+
+
+print(f"sin({input_angle}) => {math.sin(angle)}")
+
+
+
+
