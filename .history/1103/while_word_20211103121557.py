17a45221c0dcf5bb8f4751ac60af218ffc6bb771
Yuto Higuchi <gutchan915@gmail.com>
2021-11-03 12:22:46 +0900

    comit

---
+++ .history/1103/while_word_20211103121557.py
@@ -0,0 +1,42 @@
+'''
+
+【実行例】
+> python while_word.py
+単語を入力してください： hello
+単語を入力してください： world
+単語を入力してください： LIST
+単語リスト： ['hello', 'world']
+単語を入力してください： hello
+すでに登録済です
+単語を入力してください： Good
+単語を入力してください： bye
+単語を入力してください：
+終了します
+これまでに覚えた単語： ['hello', 'world', 'Good', 'bye']
+
+'''
+
+data =['hello', 'world']
+
+
+while True:
+
+    a = input("単語を入力してください：")
+
+    if a == "":
+        print("終了します")
+        print(f"これまでに覚えた単語：{data}")
+        break
+
+    if a in data:
+        print("すでに登録済みです")
+    else:
+        data.append(a)
+
+    if a == "LIST":
+        print(f"単語リスト：{data}")
+
+
+
+
+
---
+++ .history/1103/while_word_20211103121716.py
@@ -0,0 +1,43 @@
+'''
+
+【実行例】
+> python while_word.py
+単語を入力してください： hello
+単語を入力してください： world
+単語を入力してください： LIST
+単語リスト： ['hello', 'world']
+単語を入力してください： hello
+すでに登録済です
+単語を入力してください： Good
+単語を入力してください： bye
+単語を入力してください：
+終了します
+これまでに覚えた単語： ['hello', 'world', 'Good', 'bye']
+
+'''
+
+data =['hello', 'world']
+
+
+while True:
+
+    a = input("単語を入力してください：")
+
+    if a == "":
+        print("終了します")
+        print(f"これまでに覚えた単語：{data}")
+        break
+
+    if a == "LIST":
+        print(f"単語リスト：{data}")
+    elif a in data:
+        print("すでに登録済みです")
+    else:
+        data.append(a)
+
+
+
+
+
+
+
