36ccb06c3bb41e6134fb91b02f971ffd8b93758e
Yuto Higuchi <gutchan915@gmail.com>
2021-11-10 12:49:01 +0900

    comit

--- .history/1110/avg_20211110124554.py
+++ .history/1110/avg_20211110124554.py
@@ -14,20 +14,16 @@ person_data = []
 
 # fileの読み込み
 with open(DATA_FILENAME, 'r', encoding='utf-8') as f:
-    title = f.readline().strip().split(',')                                         #1行目を読み込み
-    for line in f:                                                                  #残りの行を全て読み込み
+    title = f.readline().strip().split(',') #1行目を読み込み
+    for line in f:  #残りの行を全て読み込み
         person_data.append(line.strip().split(','))
 
-
-print('person_dataは', person_data)
-
-
-col = 1                                                                             #[0]は名前なのでSkip[1]から
-avg = [0] * len(title)                                                              #平均を格納するリスト
+col = 1     #[0]は名前なのでSkip[1]から
+avg = [0] * len(title)      #平均を格納するリスト
 while col < len(title):
-    for score in person_data:                                                       #全員分の科目得点の処理
+    for score in person_data:       #全員分の科目得点の処理
         avg[col] += int(score[col])/len(person_data)
-    col += 1                                                                        #次の科目
+    col += 1    #次の科目
 
 print(title[1:])
 print(avg[1:])
