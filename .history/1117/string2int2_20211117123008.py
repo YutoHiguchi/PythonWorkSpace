343098a518b31749476b2dc61a6e8a529ceee782
Yuto Higuchi <gutchan915@gmail.com>
2021-11-17 12:34:22 +0900

    comit

--- .history/1117/string2int2_20211117123008.py
+++ .history/1117/string2int2_20211117123008.py
@@ -8,28 +8,6 @@
 
 '''
 
-def str2int(s):
-    """str2int
-    文字列を数値に変換した値を返す
-    """
-
-    if type(s) is str:  # 文字列かどうか確認
-
-        # 文字列の時
-        if s.isnumeric():   # 数値かどうかの確認
-            return int(s)   # true
-
-        else:
-            return 0    # false
-
-    else:
-        return s    # 文字列ではなかった時
-
-print(str2int('a'))
-print(str2int('10'))
-print(str2int(100))
-
-
 
 def list2int(s):
     """list2int
