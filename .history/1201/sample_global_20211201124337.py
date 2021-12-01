40cb259a55471cecdcbab9ec698b66cd0032ef62
Yuto Higuchi <gutchan915@gmail.com>
2021-12-01 12:47:26 +0900

    comit

---
+++ .history/1201/sample_global_20211201124337.py
@@ -0,0 +1,68 @@
+var_global = 100
+
+def func():
+    print(var_global)
+    var_local = 'A' # var_global = 'A' はどうなる？
+    print(var_local)
+
+print(var_global)
+func()
+print(var_local)
+
+
+
+
+
+'''
+
+【実行結果】
+> python vote.py
+票の数は 0 です vote_box: []
+投票します
+投票します
+投票します
+投票します
+投票します
+投票します
+投票します
+投票します
+投票します
+投票します
+票の数は 10 です vote_box: ['票', '票', '票', '票', '票', '票', '票', '票',
+'票', '票']
+箱を空にします
+投票します
+投票します
+票の数は 2 です vote_box: ['票', '票']
+
+'''
+
+
+
+# P.168
+vote_box = []
+label = '票'
+
+def vote(var_global):
+    print('投票します')
+    vote_box.append(label)
+
+def reset_box():
+    print('箱を空にします')
+    vote_box.clear()
+
+def check_box():
+    print('票の数は{}です'.format(len(vote_box)), end=" ")
+    print('vote_box:', vote_box)
+
+
+# main3
+check_box()
+for i in range(10):
+    vote()
+
+check_box()
+reset_box()
+vote()
+vote()
+check_box()
--- .history/1201/unc_any_20211201122615.py
+++ .history/1201/unc_any_20211201122615.py
@@ -21,18 +21,9 @@
 
 
 def out_csvdata(**kwargs):
-    out_list = []
-    for token in ['B', 'L', 'D']:
-        if token in kwargs.keys():
-            out_list.append(kwargs[token])
-        else:
-            out_list.append('-')
+    eat =
 
-    # print(out_list)
-    print(",".join(out_list))
-
-
-# main
+    # main
 eat = {}
 while True:
     menu = input("朝食(B) 昼食(L) 夕食(D)と食べたものを入力してください： ")
