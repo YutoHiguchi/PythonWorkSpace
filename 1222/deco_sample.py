6785ea34da52edf5984d2639445e2891ecb5d945
Yuto Higuchi <gutchan915@gmail.com>
2021-12-22 10:12:45 +0900

    comit

---
+++ 1222/deco_sample.py
@@ -0,0 +1,46 @@
+'''
+
+【実行結果】
+> python deco_sample.py
+test->10
+[start]--
+test->20
+--[end]
+[start]--
+test->30
+--[end]
+[start]--
+test2->100
+--[end]
+
+'''
+
+
+def startend(func):
+    def funcname(*args, **kwargs):
+        print('[start]--')
+        reslut = func(*args, **kwargs)
+        print('--[end]')
+        return reslut
+    return funcname
+
+def test(n):
+    print('test->{}'.format(n))
+
+# main
+test(10)  # 通常の呼出
+
+# デコレータ使用
+deco_func = startend(test)
+deco_func(20)
+
+# 同一の関数名でデコレータ使用
+test = startend(test)
+test(30)
+
+# @記法を使用
+@startend
+def test2(n):
+    print('test2->{}'.format(n))
+
+test2(100)
