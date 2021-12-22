85dc313029e9b8ca6dc20fef03b2f49981d699c1
Yuto Higuchi <gutchan915@gmail.com>
2021-12-22 10:54:09 +0900

    comit

---
+++ .history/1222/deco_log_20211222101743.py
@@ -0,0 +1,19 @@
+'''
+
+【実行例：画面】
+test->100
+test->0
+test->1
+test->2
+test->3
+test->4
+
+【実行例：python.log】
+2020-06-10 18:57:40.079496 function:test args:(100,) kwargs:{}
+2020-06-10 18:57:40.079570 function:test args:(0,) kwargs:{}
+2020-06-10 18:57:40.079598 function:test args:(1,) kwargs:{}
+2020-06-10 18:57:40.079617 function:test args:(2,) kwargs:{}
+2020-06-10 18:57:40.079635 function:test args:(3,) kwargs:{}
+2020-06-10 18:57:40.079653 function:test args:(4,) kwargs:{}
+
+'''
---
+++ .history/1222/deco_log_20211222105007.py
@@ -0,0 +1,50 @@
+'''
+
+【実行例：画面】
+test->100
+test->0
+test->1
+test->2
+test->3
+test->4
+
+【実行例：python.log】
+2020-06-10 18:57:40.079496 function:test args:(100,) kwargs:{}
+2020-06-10 18:57:40.079570 function:test args:(0,) kwargs:{}
+2020-06-10 18:57:40.079598 function:test args:(1,) kwargs:{}
+2020-06-10 18:57:40.079617 function:test args:(2,) kwargs:{}
+2020-06-10 18:57:40.079635 function:test args:(3,) kwargs:{}
+2020-06-10 18:57:40.079653 function:test args:(4,) kwargs:{}
+
+'''
+
+
+import datetime
+def log_file(func):
+    def funcname(*args, **kwargs):
+        # ファイルを使用します
+        LOG_FILENAME = 'python.log'
+        #現在の日付と日時
+        dt_now = datetime.datetime.now();
+        #   ファイル操作
+        with open(LOG_FILENAME, 'a') as f:
+            f.write(f'{dt_now}function:{func.__name__}args:{args}kwargs:(kwargs)\n')
+
+        result =  func(*args, **kwargs)
+        return result
+    return funcname
+# dt_now = datetime.datetime.now()  日時取得
+
+# main
+@log_file
+def test(n):
+    print('test->{}'.format(n))
+
+# 呼出
+test(100)
+
+# 呼出
+for i in range(5):
+    test(i)
+
+
