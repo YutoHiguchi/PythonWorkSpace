d91630aec03f122d46e2984d40ade02542de8389
Yuto Higuchi <gutchan915@gmail.com>
2021-12-22 11:53:13 +0900

    comit

---
+++ .history/1222/string2int_20211222114912.py
@@ -0,0 +1,28 @@
+'''
+
+【実行結果】
+> python string2int.py
+0
+10
+100
+
+'''
+
+
+def str2int(s):
+    """str2int 文字列を数値に変換した値を返す"""
+
+    if(s.isnumeric()){
+        return s
+
+    }else{
+        return str(s)
+    }
+
+
+
+
+
+print(str2int('a'))
+print(str2int('10'))
+print(str2int(100))
